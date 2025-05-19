765590294e33b9e8467c6f8be96e3507ebe82501
Ozuruigbo Tochukwu <ozuruigbotochukwu3@gmail.com>
2025-05-19 08:11:20 +0330

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './Contact.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import mail_icon from '../../assets/mail_icon.svg'
+import location_icon from '../../assets/location_icon.svg'
+import call_icon from '../../assets/call_icon.svg'
+
+const Contact = () => {
+  return (
+    <div id='contact' className='contact'>
+        <div className="contact-title">
+            <h1>Get in Touch</h1>
+            <img src={theme_pattern} alt="" />
+        </div>
+        <div className="contact-section">
+            <div className="contact-left">
+                <h1>Let's talk</h1>
+                <p>I'm currrently available to take on new projects.</p>
+                <div className="contact-details">
+                    <div className="contact-detail">
+                        <img src={mail_icon} alt="" /><p>sotidea01@gmail.com</p>
+
+                    </div>
+                    <div className="contact-detail">
+                        <img src={location_icon} alt="" /><p>0816569981</p>
+                        
+                    </div>
+                    <div className="contact-detail">
+                        <img src={call_icon} alt="" /><p>Faringada, Jos Plateau State, </p>
+                    </div>
+                </div>
+            </div>
+            <form action="https://formspree.io/f/xeoaoega" method='POST' className='contact-right'>
+                <label htmlFor="">Your Name</label>
+                <input type='text' placeholder='Enter your name' name='name'  />
+                <label htmlFor="">Your Email</label>
+                <input type='email' placeholder='Enter your email' name='email'  />
+                <label htmlFor="">Write your message here</label>
+                <textarea name='message' rows='8' placeholder='Enter your message'></textarea>
+                <button type='submit' className="contact-submit">Submit Now</button>
+
+
+            </form>
+        </div>
+
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+
+const Hero = () => {
+  return (
+    <div id='home' className='hero'>
+        <img src={profile_img} alt="" />
+        <h1><span>I am Anthony,</span> frontend developer based in Nigeria</h1>
+        <p>I am a developer from Plateau, NGA with 8 years of experience.</p>
+        <div className="hero-action">
+            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
+            <div className="hero-resume">My resume</div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
