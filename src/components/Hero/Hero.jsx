38f5263359673326c72ad7b7da0f9d23f4580c48
MiracleKalu <miracle@rarque.com>
2025-06-18 21:15:39 +0100

    log and work images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import "./Hero.css";
+import studio_2 from "../../assets/images/studio_2.jpg";
 
-
+import AnchorLink from "react-anchor-link-smooth-scroll";
+ 
 const Hero = () => {
   return (
-    <div id='home' className='hero'>
-        <img src={profile_img} alt="" />
-        <h1><span>I am Anthony,</span> frontend developer based in Nigeria</h1>
-        <p>I am a developer from Plateau, NGA with 8 years of experience.</p>
-        <div className="hero-action">
-            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-            <div className="hero-resume">My resume</div>
+    <div id="home" className="hero">
+      <img src={studio_2} alt="" />
+      <h1>
+        <span>I am Anthony,</span> frontend developer based in Nigeria
+      </h1>
+      <p>I am a developer from Plateau, NGA with 8 years of experience.</p>
+      <div className="hero-action">
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me
+          </AnchorLink>
         </div>
+        <div className="hero-resume">My resume</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
